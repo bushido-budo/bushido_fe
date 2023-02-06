b570adb6d84760c6473cf43df0e1e1f572ceef3e
Anthony Shellman <atshellman@gmail.com>
2023-02-06 16:22:09 -0700

    Add footer contents, including placeholders

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,13 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer>
-      Footer Placeholder
+      <div>
+        <p className='focus-text'>集中</p>
+      </div>
+      <div>
+        <p className='anthony-info'>Info Placeholder 1</p>
+        <p className='ben-info'>Info Placeholder 2</p>
+      </div>
     </footer>
   )
 }
