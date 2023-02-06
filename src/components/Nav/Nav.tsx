d0c8b175940d5ed71ccf7a45bb259ef5f5bade9e
Anthony Shellman <atshellman@gmail.com>
2023-02-05 19:52:21 -0700

    Initial commit

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,13 @@
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className='links-container'>
+        NavBar Stuff
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
