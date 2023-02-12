138fd3cc24dc45cd273ac3ff5030fe6ebffde0c6
Anthony Shellman <atshellman@gmail.com>
2023-02-11 19:15:23 -0700

    Add various containers and sections

--- src/components/StudentDashboard/StudentDashboard.tsx
+++ src/components/StudentDashboard/StudentDashboard.tsx
@@ -1,7 +1,23 @@
 import './StudentDashboard.css'
 
 const StudentDashboard = () => {
+  return (
+    <div className='sections-container'>
+      <div className='upper-sections-container'>
+      <section className='student-profile'>
 
+      </section>
+      <section className='student-metrics'>
+
+      </section>
+      </div>
+      <div className='lower-section-container'>
+      <section className='student-techniques'>
+
+      </section>
+      </div>
+    </div>
+  )
 }
 
 export default StudentDashboard
