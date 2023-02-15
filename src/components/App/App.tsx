c79fbe6580c69255007d48949080bde8642cb21e
Anthony Shellman <atshellman@gmail.com>
2023-02-14 17:44:05 -0700

    Uncomment import statments and components - add routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from '../Header/Header';
-// import LandingPage from '../LandingPage/LandingPage';
-// import StudentDashboard from '../StudentDashboard/StudentDashboard';
+import LandingPage from '../LandingPage/LandingPage';
+import StudentDashboard from '../StudentDashboard/StudentDashboard';
 import AdminDashboard from '../AdminDashboard/AdminDashboard';
 import Footer from '../Footer/Footer';
 import './App.css';
@@ -9,10 +10,12 @@ const App = () => {
   return (
       <main>
         <Header />
-        {/* <LandingPage /> */}
-        {/* <StudentDashboard /> */}
-        <AdminDashboard />
-        {/* <Footer /> */}
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/dashboard/student_id' element={<StudentDashboard />} />
+          <Route path='/dashboard/admin' element={<AdminDashboard />} />
+        </Routes>
+        <Footer />
       </main>
   );
 }
