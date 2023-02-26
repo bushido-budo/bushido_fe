1255d6eac274090bdc1ed3d5a8ae77aeb199c8aa
Anthony Shellman <atshellman@gmail.com>
2023-02-25 18:18:48 -0700

    Add data to render as test - encountered bug; will research

--- src/components/StudentDashboard/StudentDashboard.tsx
+++ src/components/StudentDashboard/StudentDashboard.tsx
@@ -7,11 +7,19 @@ interface StudentDashboardProps {
 }
 
 const StudentDashboard: FC<StudentDashboardProps> = ({ studentInfo }) => {
+
+  // Note: studentInfo.data.blah info breaks on page student dashboard page refresh
+  // will research local storage or some other way to fix the issue
+
+  // Also, should the studentInfo be passed down from App, or just loaded directly
+  // here on page load?
   return (
     <div className='student-sections-container'>
       <div className='student-upper-sections-container'>
       <section className='student-profile'>
-
+        <p>{`${studentInfo.data.first_name} ${studentInfo.data.last_name}`}</p>
+        <p>{studentInfo.data.belt_rank}</p>
+        <p>{studentInfo.data.user_status}</p>
       </section>
       <section className='student-metrics'>
 
