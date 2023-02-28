e9da9f5155cd61e3b6f5b4b381085022d83d3d21
Anthony Shellman <atshellman@gmail.com>
2023-02-27 19:28:27 -0700

    Render metrics data, some of current materials data

--- src/components/StudentDashboard/StudentDashboard.tsx
+++ src/components/StudentDashboard/StudentDashboard.tsx
@@ -18,12 +18,39 @@ const StudentDashboard: FC<StudentDashboardProps> = ({ studentInfo }) => {
         { studentInfo.data && <p>Status: {studentInfo.data.user_status}</p> }
       </section>
       <section className='student-metrics'>
-
+        <p className='metrics-header'>Metrics</p>
+          <p>Jump Rope</p> 
+          { studentInfo.data && <p>Your Avg: {studentInfo.data.belt_test_metrics.student_average.jump_rope} </p> }
+          { studentInfo.data && <p>Dojo Avg: {studentInfo.data.belt_test_metrics.dojo_average.jump_rope} </p> }
+        <p>Sit Ups</p>
+          { studentInfo.data && <p>Your Avg: {studentInfo.data.belt_test_metrics.student_average.sit_ups}</p> }
+          { studentInfo.data && <p>Dojo Avg: {studentInfo.data.belt_test_metrics.dojo_average.sit_ups} </p> }
+        <p>Balanced Kicks</p>
+          { studentInfo.data && <p>Your Avg: {studentInfo.data.belt_test_metrics.student_average.balanced_kicks}</p> }
+          { studentInfo.data && <p>Dojo Avg: {studentInfo.data.belt_test_metrics.dojo_average.balanced_kicks} </p> }
+        <p>Lunge Pumps</p>
+          { studentInfo.data && <p>Your Avg: {studentInfo.data.belt_test_metrics.student_average.lunge_pumps}</p> }
+          { studentInfo.data && <p>Dojo Avg: {studentInfo.data.belt_test_metrics.dojo_average.lunge_pumps} </p> }
       </section>
       </div>
       <div className='student-lower-section-container'>
       <section className='student-techniques'>
-
+        <div className='current-materials'>
+          <div >
+            { studentInfo.data && <p> {Object.keys(studentInfo.data.current_material)[0]}</p>}
+            { studentInfo.data && studentInfo.data.current_material.blocking_system.hammer.map(val => <p key={val}>{val}</p>)}
+          </div>
+          { studentInfo.data && <p> {studentInfo.data && Object.keys(studentInfo.data.current_material.blocking_system)[1]}</p>}
+          { studentInfo.data && <p> {studentInfo.data && Object.keys(studentInfo.data.current_material)[1]}</p>}
+          { studentInfo.data && <p> {studentInfo.data && Object.keys(studentInfo.data.current_material.eight_point_knife_counter)[0]}</p>}
+          { studentInfo.data && <p> {studentInfo.data && Object.keys(studentInfo.data.current_material.eight_point_knife_counter)[1]}</p>}
+          { studentInfo.data && <p> {studentInfo.data && Object.keys(studentInfo.data.current_material.eight_point_knife_counter)[2]}</p>}
+          { studentInfo.data && <p> {studentInfo.data && Object.keys(studentInfo.data.current_material.eight_point_knife_counter)[3]}</p>}
+          { studentInfo.data && <p> {studentInfo.data && Object.keys(studentInfo.data.current_material.eight_point_knife_counter)[4]}</p>}
+          { studentInfo.data && <p> {studentInfo.data && Object.keys(studentInfo.data.current_material.eight_point_knife_counter)[5]}</p>}
+          { studentInfo.data && <p> {studentInfo.data && Object.keys(studentInfo.data.current_material.eight_point_knife_counter)[6]}</p>}
+          { studentInfo.data && <p> {studentInfo.data && Object.keys(studentInfo.data.current_material.eight_point_knife_counter)[7]}</p>}
+        </div>
       </section>
       </div>
     </div>
