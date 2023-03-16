027e2616b6a6faa8cf1a48630240d7052057144a
Anthony Shellman <atshellman@gmail.com>
2023-03-15 19:32:21 -0600

    Add placeholder login configuration

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -1,7 +1,33 @@
-import './Login.css'
+import loginImage from "../../assets/nguyen-hung-R1ftFeTKEbk-unsplash.jpg"
+import './LogIn.css'
 
 const Login = () => {
-
+  return (
+    <section className='login-container'>
+      <div className='login'>
+        <p className='login-banner'>Welcome, Please Log In</p>
+        <form className='form-container'>
+          <input 
+            type='input'
+            title='username'
+            placeholder='Username'
+          />
+          <input 
+            type='input'
+            title='password'
+            placeholder='Password'
+          />
+          <button>Login</button>
+        </form>
+        <div>
+          <button>Create Account</button>
+        </div>
+      </div>
+      <div className='image-container'>
+          <img className="karate-image" src={loginImage} alt="person in striking karate pose"/>
+      </div>
+    </section>
+  )
 }
 
 export default Login
